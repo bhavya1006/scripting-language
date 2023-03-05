6f12f5071f2f1c52c06789a4ec5b159af7cd2f57
Bhavya Madan <121045636+bhavya1006@users.noreply.github.com>
2023-03-05 20:50:52 +0530

    Update ProductOfTwoNum.cpp

--- Assignment 1/ProductOfTwoNum.cpp
+++ Assignment 1/ProductOfTwoNum.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main(){
     int x=2,y=4;
     cout<<"Product of "<<x<<" and "<<y<<" is "<<x*y;
-
+    
+    //example calculating area of triangle
+    int l,b;
+    cout<<"Enter the length and breadth: ";
+    cin>>l>>b;
+    cout<<"Area of Rectangle: "<<l*b;
+    
     return 0;
 }
